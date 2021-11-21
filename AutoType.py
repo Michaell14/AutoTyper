8101ef0ffa18e9dbeab82b13abc5f857d78899de
Michael Li <66749877+Michaell14@users.noreply.github.com>
2021-11-21 00:16:43 -0500

    main

---
+++ AutoType.py
@@ -0,0 +1,59 @@
+import pyautogui, time
+import PySimpleGUI as sg
+
+sg.theme("DarkAmber")
+
+#1 = Write
+#2 = Press a key
+action=[]
+values=[]
+numCombo=0
+
+layout1=[[sg.Text("How many iterations of the combination: ")],
+        [sg.Text("Enter: "), sg.InputText()],
+        [sg.OK(), sg.Cancel()]]
+layout2=[[sg.Text("1) Write (What to type on the screen)\n2) Key (What to press)")],
+        [sg.Text("Option"), sg.InputText(do_not_clear=False)],
+        [sg.Text("Value: "), sg.InputText(do_not_clear=False)],
+        [sg.Text("Amount: "), sg.InputText(do_not_clear=False)],
+        [sg.OK(), sg.Cancel()]]
+
+window=sg.Window("Spammer.py", layout1)
+
+event, vals=window.read()
+numCombo = int(vals[0])
+window.close()
+
+window=sg.Window("Spammer.py", layout2)
+while True:
+    event, vals=window.read()
+    if event == sg.WIN_CLOSED or event == 'Cancel': # if user closes window or clicks cancel
+        break
+    action.append(int(vals[0]))
+    values.append([vals[1], int(vals[2])])
+
+window.close()
+print(action)
+print(values)
+
+print("STARTING...")
+
+
+def main():
+    for i in range(len(values)):
+        val=values[i][0]
+
+        for x in range(values[i][1]):
+            if action[i]==1:
+                pyautogui.write(val)
+            else:
+                pyautogui.press(val)
+                time.sleep(.5)
+
+for i in range(5, 0, -1):
+    print(i)
+    time.sleep(1)
+
+for i in range(numCombo):
+    main()
+    time.sleep(1)
